dbb050ce539fe60334b4d2634f8d099b1fc20768
William Hewell <whe@fairfaxmedia.com.au>
2013-08-04 02:59:44 +1000

    model layrt

---
+++ app/models/User.java
@@ -0,0 +1,36 @@
+package models;
+
+import play.db.ebean.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hewell
+ * Date: 3/08/13
+ * Time: 8:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+public class User extends Model {
+    @Id
+    public String email;
+    public String name;
+    public String password;
+
+    public User(String email, String name, String password) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
+
+    public static Finder<String, User> find = new Finder<String, User>(
+            String.class, User.class
+    );
+
+    public static User authenticate(String email, String password) {
+        return find.where().eq("email", email)
+                .eq("password", password).findUnique();
+    }
+}
